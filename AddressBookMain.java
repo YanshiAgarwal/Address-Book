5891679dc732c6cfc12277ddefc6fb194c93b5d0
Yanshi Agarwal <aparul742@gmail.com>
2026-07-02 12:26:31 +0530

    [Yanshi] Added UC1: Create Contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,7 +1,27 @@
 public class AddressBookMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Address Book System");
+        System.out.println("Welcome to Address Book Program in Java");
+    
+        Contact person = new Contact();
+
+        person.firstName = "Yanshi";
+        person.lastName = "Agarwal";
+        person.address = "Mathura Road";
+        person.city = "Mathura";
+        person.state = "Uttar Pradesh";
+        person.zip = "281001";
+        person.phoneNumber = "9876543210";
+        person.email = "yanshi@gmail.com";
+
+        System.out.println("First Name : " + person.firstName);
+        System.out.println("Last Name : " + person.lastName);
+        System.out.println("Address : " + person.address);
+        System.out.println("City : " + person.city);
+        System.out.println("State : " + person.state);
+        System.out.println("Zip : " + person.zip);
+        System.out.println("Phone Number : " + person.phoneNumber);
+        System.out.println("Email : " + person.email);
     }
 
 }
---
+++ Contact.java
@@ -0,0 +1,12 @@
+public class Contact {
+
+    String firstName;
+    String lastName;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNumber;
+    String email;
+
+}
