640c13973670282a00833688d17e0657bf8c9828
Yanshi Agarwal <aparul742@gmail.com>
2026-07-02 13:02:02 +0530

    [Yanshi] Added UC3: Edit existing contact

--- AddressBook.java
+++ AddressBook.java
@@ -1,5 +1,87 @@
+import java.util.Scanner;
+
 public class AddressBook {
 
-     Contact person = new Contact();
-    
+    Contact person = new Contact();
+    Scanner sc = new Scanner(System.in);
+
+    // UC2
+    public void addContact() {
+
+        System.out.print("Enter First Name: ");
+        person.firstName = sc.nextLine();
+
+        System.out.print("Enter Last Name: ");
+        person.lastName = sc.nextLine();
+
+        System.out.print("Enter Address: ");
+        person.address = sc.nextLine();
+
+        System.out.print("Enter City: ");
+        person.city = sc.nextLine();
+
+        System.out.print("Enter State: ");
+        person.state = sc.nextLine();
+
+        System.out.print("Enter Zip: ");
+        person.zip = sc.nextLine();
+
+        System.out.print("Enter Phone Number: ");
+        person.phoneNumber = sc.nextLine();
+
+        System.out.print("Enter Email: ");
+        person.email = sc.nextLine();
+
+        System.out.println("Contact Added Successfully!");
+    }
+
+    // UC3
+    public void editContact() {
+
+        System.out.print("\nEnter First Name to Edit: ");
+        String name = sc.nextLine();
+
+        if (person.firstName.equals(name)) {
+
+            System.out.println("Contact Found!");
+
+            System.out.print("Enter New Address: ");
+            person.address = sc.nextLine();
+
+            System.out.print("Enter New City: ");
+            person.city = sc.nextLine();
+
+            System.out.print("Enter New State: ");
+            person.state = sc.nextLine();
+
+            System.out.print("Enter New Zip: ");
+            person.zip = sc.nextLine();
+
+            System.out.print("Enter New Phone Number: ");
+            person.phoneNumber = sc.nextLine();
+
+            System.out.print("Enter New Email: ");
+            person.email = sc.nextLine();
+
+            System.out.println("Contact Updated Successfully!");
+
+        } else {
+
+            System.out.println("Contact Not Found!");
+
+        }
+    }
+
+    public void displayContact() {
+
+        System.out.println("\n----- Contact Details -----");
+        System.out.println("First Name : " + person.firstName);
+        System.out.println("Last Name : " + person.lastName);
+        System.out.println("Address : " + person.address);
+        System.out.println("City : " + person.city);
+        System.out.println("State : " + person.state);
+        System.out.println("Zip : " + person.zip);
+        System.out.println("Phone : " + person.phoneNumber);
+        System.out.println("Email : " + person.email);
+    }
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,50 +1,18 @@
-import java.util.Scanner;
-
 public class AddressBookMain {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Address Book System");
-
-        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Address Book Program in Java");
 
         AddressBook addressBook = new AddressBook();
 
-        System.out.print("Enter First Name: ");
-        addressBook.person.firstName = sc.nextLine();
+        // UC2
+        addressBook.addContact();
 
-        System.out.print("Enter Last Name: ");
-        addressBook.person.lastName = sc.nextLine();
+        // UC3
+        addressBook.editContact();
 
-        System.out.print("Enter Address: ");
-        addressBook.person.address = sc.nextLine();
-
-        System.out.print("Enter City: ");
-        addressBook.person.city = sc.nextLine();
-
-        System.out.print("Enter State: ");
-        addressBook.person.state = sc.nextLine();
-
-        System.out.print("Enter Zip: ");
-        addressBook.person.zip = sc.nextLine();
-
-        System.out.print("Enter Phone Number: ");
-        addressBook.person.phoneNumber = sc.nextLine();
-
-        System.out.print("Enter Email: ");
-        addressBook.person.email = sc.nextLine();
-
-        System.out.println("\nContact Added Successfully!");
-
-        System.out.println("First Name: " + addressBook.person.firstName);
-        System.out.println("Last Name: " + addressBook.person.lastName);
-        System.out.println("Address: " + addressBook.person.address);
-        System.out.println("City: " + addressBook.person.city);
-        System.out.println("State: " + addressBook.person.state);
-        System.out.println("Zip: " + addressBook.person.zip);
-        System.out.println("Phone: " + addressBook.person.phoneNumber);
-        System.out.println("Email: " + addressBook.person.email);
-
-        sc.close();
+        // Display updated contact
+        addressBook.displayContact();
     }
 }
